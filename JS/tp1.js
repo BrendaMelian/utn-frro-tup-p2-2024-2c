c857aee59eda012712bfe2ce27efb88ae4be5aa1
BrendaMelian <meliansalernobrenda@gmail.com>
2024-09-04 22:56:21 -0300

    Actualiza ejercicios JS

---
+++ JS/tp1.js
@@ -0,0 +1,34 @@
+console.log("Hola mundo!");
+document.write("Hola MUNDO!");
+document.write(" ");
+document.write(3+5);
+
+var nombre = prompt("Ingrese su nombre: ", "");
+document.write(" Hola, " + nombre);
+
+var num1 = prompt("Ingrese un numero: ", 0);
+var num2 = prompt("Ingrese otro numero: ", 0);
+
+num1=parseInt(num1);
+num2=parseInt(num2);
+
+var suma=0;
+var suma= num1 + num2;
+document.write("La suma de los numeros es: ", suma);
+
+var resta=0;
+var resta= num1 - num2;
+document.write("La resta de los numeros es: ", resta);
+
+var mult=0;
+var mult= num1 * num2;
+document.write("La multiplicacion de los numeros es: ", mult);
+
+var divi=0;
+var divi= num1 / num2;
+document.write("La division de los numeros es: ", divi);
+
+
+
+
+
